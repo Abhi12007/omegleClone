0a3f3517508821b9b804acdd6c95654e2e7ecff3
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-01 18:39:31 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,17 +7,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*", methods: ["GET","POST"] } });
 
+// pairing
 const waitingQueue = [];
 const partners = {};
-const userInfo = {}; // socketId -> {name, gender}
+const userInfo = {}; // socketId -> { name, gender }
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
+
+  // emit both event names so clients listening for either will work
   io.emit('online-count', io.engine.clientsCount);
+  io.emit('online-users', io.engine.clientsCount);
 
   socket.on('join', (data) => {
-    // data: {name, gender}
-    userInfo[socket.id] = data || {name: 'Anonymous', gender: 'other'};
+    // data: { name, gender }
+    userInfo[socket.id] = data || { name: 'Anonymous', gender: 'other' };
     if (partners[socket.id]) return;
     if (waitingQueue.length === 0) {
       waitingQueue.push(socket.id);
@@ -26,41 +30,62 @@ io.on('connection', (socket) => {
       const peerId = waitingQueue.shift();
       partners[peerId] = socket.id;
       partners[socket.id] = peerId;
-      // send partner info to both
+
+      // send partner info to both peers (so client can display name/gender)
       io.to(peerId).emit('paired', { partnerId: socket.id, initiator: true, partnerInfo: userInfo[socket.id] });
       io.to(socket.id).emit('paired', { partnerId: peerId, initiator: false, partnerInfo: userInfo[peerId] });
+
+      // update online counts for everyone (still valid)
+      io.emit('online-count', io.engine.clientsCount);
+      io.emit('online-users', io.engine.clientsCount);
+
+      console.log('Paired', peerId, '<->', socket.id);
     }
   });
 
+  // signaling
   socket.on('offer', ({ to, sdp }) => { if (to) io.to(to).emit('offer', { from: socket.id, sdp }); });
   socket.on('answer', ({ to, sdp }) => { if (to) io.to(to).emit('answer', { from: socket.id, sdp }); });
   socket.on('ice-candidate', ({ to, candidate }) => { if (to) io.to(to).emit('ice-candidate', { from: socket.id, candidate }); });
 
-  // chat
+  // chat forwarding (include sender's name)
   socket.on('chat-message', ({ to, message }) => {
-    if (to) {
-      // forward message and include sender's name
-      const info = userInfo[socket.id] || {name: 'Anonymous', gender: 'other'};
-      io.to(to).emit('chat-message', { from: socket.id, fromName: info.name, message });
-    }
+    if (!to) return;
+    const info = userInfo[socket.id] || { name: 'Stranger', gender: 'other' };
+    io.to(to).emit('chat-message', { from: socket.id, fromName: info.name, message });
   });
 
   socket.on('leave', () => {
     const partner = partners[socket.id];
-    if (partner) { io.to(partner).emit('partner-left'); delete partners[partner]; }
-    else { const idx = waitingQueue.indexOf(socket.id); if (idx !== -1) waitingQueue.splice(idx,1); }
+    if (partner) {
+      io.to(partner).emit('partner-left');
+      delete partners[partner];
+    } else {
+      const idx = waitingQueue.indexOf(socket.id);
+      if (idx !== -1) waitingQueue.splice(idx, 1);
+    }
     delete partners[socket.id];
     delete userInfo[socket.id];
+
     io.emit('online-count', io.engine.clientsCount);
+    io.emit('online-users', io.engine.clientsCount);
   });
 
   socket.on('disconnect', () => {
     const partner = partners[socket.id];
-    if (partner) { io.to(partner).emit('partner-left'); delete partners[partner]; }
-    else { const idx = waitingQueue.indexOf(socket.id); if (idx !== -1) waitingQueue.splice(idx,1); }
+    if (partner) {
+      io.to(partner).emit('partner-left');
+      delete partners[partner];
+    } else {
+      const idx = waitingQueue.indexOf(socket.id);
+      if (idx !== -1) waitingQueue.splice(idx, 1);
+    }
     delete partners[socket.id];
     delete userInfo[socket.id];
+
     io.emit('online-count', io.engine.clientsCount);
+    io.emit('online-users', io.engine.clientsCount);
+    console.log('User disconnected:', socket.id);
   });
 });
 
