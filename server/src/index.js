757c52d81e88de245529ec51e2f272f91657c38d
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-01 21:09:11 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -8,11 +8,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*", methods: ["GET","POST"] } });
 
-const waitingQueue = []; // list of socket ids
-const partners = {};     // socketId -> partnerSocketId
-const userInfo = {};     // socketId -> { name, gender }
+const waitingQueue = [];
+const partners = {};
+const userInfo = {};
 
-// helper to remove socket from waitingQueue
 function removeFromQueue(socketId) {
   const idx = waitingQueue.indexOf(socketId);
   if (idx !== -1) waitingQueue.splice(idx, 1);
@@ -21,24 +20,15 @@ function removeFromQueue(socketId) {
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // broadcast counts
   io.emit('online-count', io.engine.clientsCount);
   io.emit('online-users', io.engine.clientsCount);
 
-  // join: data = { name, gender }
   socket.on('join', (data) => {
-    // store user info
     userInfo[socket.id] = data || { name: 'Anonymous', gender: 'other' };
+    if (partners[socket.id]) return;
 
-    // do not add if already paired
-    if (partners[socket.id]) {
-      return;
-    }
-
-    // remove if somehow exists (prevent duplicates)
     removeFromQueue(socket.id);
 
-    // if no one waiting, add to queue and tell them to wait
     if (waitingQueue.length === 0) {
       waitingQueue.push(socket.id);
       socket.emit('waiting');
@@ -47,7 +37,6 @@ io.on('connection', (socket) => {
       return;
     }
 
-    // find a random waiting peer that is not the same and still connected
     let peerIndex = -1;
     for (let i = 0; i < waitingQueue.length; i++) {
       const cand = waitingQueue[i];
@@ -58,56 +47,42 @@ io.on('connection', (socket) => {
     }
 
     if (peerIndex === -1) {
-      // fallback: no valid peer found, push to queue
       waitingQueue.push(socket.id);
       socket.emit('waiting');
-      io.emit('online-count', io.engine.clientsCount);
-      io.emit('online-users', io.engine.clientsCount);
       return;
     }
 
-    // remove chosen peer from queue
     const peerId = waitingQueue.splice(peerIndex, 1)[0];
 
-    // pair them
     partners[peerId] = socket.id;
     partners[socket.id] = peerId;
 
-    // send partner info to both sides
     io.to(peerId).emit('paired', { partnerId: socket.id, initiator: true, partnerInfo: userInfo[socket.id] });
     io.to(socket.id).emit('paired', { partnerId: peerId, initiator: false, partnerInfo: userInfo[peerId] });
-
-    io.emit('online-count', io.engine.clientsCount);
-    io.emit('online-users', io.engine.clientsCount);
-
-    console.log('Paired', peerId, '<->', socket.id);
   });
 
-  // signaling
   socket.on('offer', ({ to, sdp }) => { if (to) io.to(to).emit('offer', { from: socket.id, sdp }); });
   socket.on('answer', ({ to, sdp }) => { if (to) io.to(to).emit('answer', { from: socket.id, sdp }); });
   socket.on('ice-candidate', ({ to, candidate }) => { if (to) io.to(to).emit('ice-candidate', { from: socket.id, candidate }); });
 
-  // chat
   socket.on('chat-message', ({ to, message }) => {
     if (!to) return;
     const info = userInfo[socket.id] || { name: 'Stranger', gender: 'other' };
     io.to(to).emit('chat-message', { from: socket.id, fromName: info.name, message });
   });
 
-  // leave
   socket.on('leave', () => {
     const partner = partners[socket.id];
     if (partner) {
-      // notify partner and clean mapping
       io.to(partner).emit('partner-left');
+      removeFromQueue(partner);
+      waitingQueue.push(partner); // partner requeued
       delete partners[partner];
       delete partners[socket.id];
     } else {
       removeFromQueue(socket.id);
     }
     delete userInfo[socket.id];
-
     io.emit('online-count', io.engine.clientsCount);
     io.emit('online-users', io.engine.clientsCount);
   });
@@ -117,13 +92,14 @@ io.on('connection', (socket) => {
     const partner = partners[socket.id];
     if (partner) {
       io.to(partner).emit('partner-left');
+      removeFromQueue(partner);
+      waitingQueue.push(partner); // requeue partner
       delete partners[partner];
       delete partners[socket.id];
     } else {
       removeFromQueue(socket.id);
     }
     delete userInfo[socket.id];
-
     io.emit('online-count', io.engine.clientsCount);
     io.emit('online-users', io.engine.clientsCount);
   });
