139a8f5e74d5de3bd18d64c2d82061326f40a68b
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-02 02:25:20 +0530

    Create Contact.js

---
+++ client/src/Contact.js
@@ -0,0 +1,11 @@
+// client/src/Contact.js
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Contact Us</h1>
+      <p>This is a dummy Contact page. Add a contact form or details later.</p>
+    </div>
+  );
+}
