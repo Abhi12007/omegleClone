75897c14dd70fec2dd74b5c7c64ce2fd48250223
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-02 02:24:38 +0530

    Create About.js

---
+++ client/src/About.js
@@ -0,0 +1,11 @@
+// client/src/About.js
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>About Us</h1>
+      <p>This is a dummy About page. You can add details here later.</p>
+    </div>
+  );
+}
