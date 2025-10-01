59332dc8d063cf2a98ec674c14bd677471d4fa13
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-02 02:24:59 +0530

    Create Blog.js

---
+++ client/src/Blog.js
@@ -0,0 +1,11 @@
+// client/src/Blog.js
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Blog</h1>
+      <p>This is a dummy Blog page. Future blog posts will appear here.</p>
+    </div>
+  );
+}
