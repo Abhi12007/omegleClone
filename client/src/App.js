1a71aaa8595c6cb2ca5d3c8144bf75ead9f29bc6
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-02 19:13:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -514,6 +514,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
