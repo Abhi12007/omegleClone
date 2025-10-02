e6c98a99178bd6026b1b5c80a0b271a3e6c5b81d
Abhi12007 <75125407+Abhi12007@users.noreply.github.com>
2025-10-02 14:15:50 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -504,14 +504,13 @@ export default function App() {
               /* ----- LANDING ----- */
               <div className="center-card">
                 <div className="landing-header">
-                 <img src="/banner.png" alt="Banner" className="landing-banner" />
-                 <div className="landing-title">
+                  <div className="landing-title">
                     <h1>Omegle</h1>
                     <div className="sub">Online: {onlineCount}</div>
                   </div>
 
                   {/* banner from public */}
-                  
+                  <img src="/banner.png" alt="Banner" className="landing-banner" />
                 </div>
 
                 <input className="input" placeholder="Enter your name" value={name} onChange={(e)=>setName(e.target.value)} />
